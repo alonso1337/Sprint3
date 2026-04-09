35d7430ec5473ab1e6067da08de73776b4b4574e
SddIEG <artempalcev3@gmail.com>
2026-04-09 20:06:52 +0300

    THT

--- core/src/ru/samsung/gamestudio/GameSettings.java
+++ core/src/ru/samsung/gamestudio/GameSettings.java
@@ -3,4 +3,9 @@ package ru.samsung.gamestudio;
 public class GameSettings {
     public static final int SCREEN_WIDTH = 720;
     public static final int SCREEN_HEIGHT = 1280;
+    public static final float STEP_TIME = 1f / 60;
+    public static final int VELOCITY_ITERATIONS = 6;
+    public static final int POSITION_ITERATIONS = 6;
+    public static final int SHIP_WIDTH = 150;
+    public static final int SHIP_HEIGHT = 150;
 }
--- core/src/ru/samsung/gamestudio/MyGdxGame.java
+++ core/src/ru/samsung/gamestudio/MyGdxGame.java
@@ -1,9 +1,15 @@
 package ru.samsung.gamestudio;
 
+import static ru.samsung.gamestudio.GameSettings.POSITION_ITERATIONS;
+import static ru.samsung.gamestudio.GameSettings.STEP_TIME;
+import static ru.samsung.gamestudio.GameSettings.VELOCITY_ITERATIONS;
+
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -17,6 +23,20 @@ public class MyGdxGame extends Game {
 
 	public GameScreen gameScreen;
 	public World world;
+	float accumulator = 0;
+	public Vector3 touch;
+
+
+	public void stepWorld() {
+		float delta = Gdx.graphics.getDeltaTime();
+		accumulator += delta;
+
+		if (accumulator >= STEP_TIME) {
+			accumulator -= STEP_TIME;
+			world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		}
+	}
+
 
 	@Override
 	public void create() {
@@ -26,10 +46,14 @@ public class MyGdxGame extends Game {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
 
+		touch = new Vector3();
 		gameScreen = new GameScreen(this);
 
+
 		setScreen(gameScreen);
+
 	}
+
 
 	@Override
 	public void dispose() {
